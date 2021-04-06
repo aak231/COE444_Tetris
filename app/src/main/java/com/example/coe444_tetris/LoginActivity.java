912807e97c4f5e278d80b10c432f5081c3d0427c
aak231 <ahadaak231@gmail.com>
2021-04-06 12:21:41 +0400

    added the permission

--- app/src/main/java/com/example/coe444_tetris/LoginActivity.java
+++ app/src/main/java/com/example/coe444_tetris/LoginActivity.java
@@ -1,20 +1,23 @@
 package com.example.coe444_tetris;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
-
-
-
-
-
-
+    private int STORAGE_PERMISSION_CODE = 1;
 
     private EditText username;
     private EditText password;
@@ -36,8 +39,56 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         switch(v.getId())
         {
             case R.id.loginButtonID:
-                Intent game = new Intent(this, GameActivity.class);
-                startActivity(game);
+                if(ContextCompat.checkSelfPermission(LoginActivity.this,
+                        Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
+                {
+                    Toast.makeText(getApplicationContext(),"Access Granted",Toast.LENGTH_SHORT).show();
+                    Intent game = new Intent(this, GameActivity.class);
+                    startActivity(game);
+                }
+                else {
+                    requestStoragePermission();
+                }
+
+        }
+    }
+    private void requestStoragePermission()
+    {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)){
+            new AlertDialog.Builder(this)
+                    .setTitle("Permission Needed")
+                    .setMessage("This permission is needed to use the app")
+                    .setPositiveButton("Okay", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(LoginActivity.this, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},
+                                    STORAGE_PERMISSION_CODE);
+                        }
+                    })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+        }
+        else{
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},
+                    STORAGE_PERMISSION_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == STORAGE_PERMISSION_CODE)
+        {
+            if(grantResults.length >0 && grantResults[0] ==PackageManager.PERMISSION_GRANTED)
+            {
+                Toast.makeText(this, "Permission Granted!", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                Toast.makeText(this, "Permission was not granted", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 }
